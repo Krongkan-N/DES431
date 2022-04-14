ca3ae0abeb376207941344c347fe549e7f3de286
Krongkan-N <krongkan.nfols@gmail.com>
2022-04-14 14:30:05 +0700

    Finished application

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    context={
+        'user':request.user,
+        'toker':'mysecrete'
+    }
+    return render(request, 'home.html',context=context)
